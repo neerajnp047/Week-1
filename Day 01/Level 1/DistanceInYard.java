9566a38e75366fd6ad773d07aba00d6268618699
neerajnp047 <neerajnp047@gmail.com>
2025-01-10 17:16:19 +0530

    Day01

--- Day 01/Level 1/DistanceInYard.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class DistanceInYard {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter distance ");
-        float distanceInInch = sc.nextFloat();
-        System.out.print(" Your Height in yard is "+ distanceInInch/3 +" while in mile is " + distanceInInch/(3*1760)+"and inches is"+distanceInInch);
-    }
-}
